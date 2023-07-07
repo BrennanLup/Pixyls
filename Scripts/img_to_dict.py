afa3cf205ba709dd0d49679d2962517f4f21103d
Brennan Lupyrypa <brennanlupyrypa@Brennans-MacBook-Pro-2.local>
2023-07-06 21:09:58 -0400

    added dithering algorithm

--- Scripts/img_to_dict.py
+++ Scripts/img_to_dict.py
@@ -2,6 +2,15 @@ from PIL import Image
 import output_pixel_sheet
 
 
+def dither_image(image, num_colors):
+    # Convert the image to a specific mode with the desired number of colors
+    image = image.convert("P", palette=Image.Palette.ADAPTIVE, colors=num_colors)
+    # Apply dithering
+    dithered_image = image.convert("RGB", dither=Image.Dither.FLOYDSTEINBERG)
+    
+    # Display the dithered image
+    return dithered_image
+
 def count_colors(image):
     width, height = image.size
     colors = {}
@@ -22,7 +31,9 @@ def outputDict(colorDict):
 # Open the image
 # pixyl_images/pixyl_windows.jpeg
 # frog.jpg
-image = Image.open("../purp.jpeg")
+image = Image.open("../purp.png")
+image = dither_image(image, 30)
+image.show()
 
 # Count the number of colors
 num_colors = count_colors(image)
